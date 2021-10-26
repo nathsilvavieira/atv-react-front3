a9f33d2116047105c27fc7ab31b3622bf6b3acae
Nathalia Vieira <naahviera@gmail.com>
2021-10-26 12:21:51 -0300

    adicionando

---
+++ mesa-4/src/App.js
@@ -0,0 +1,18 @@
+import logo from './homem-feliz-dinheiro.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Homem muito feliz por estar em um aplicativo limpo.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
